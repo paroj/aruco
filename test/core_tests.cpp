3d77c3bc2272096a2bcc7d7894e164cf1327cef1
Pavel Rojtberg <pavel.rojtberg@igd.fraunhofer.de>
2015-06-26 19:48:17 +0200

    use opencv filestorage for serialization.
    fixes #13. add board test.

--- test/core_tests.cpp
+++ test/core_tests.cpp
@@ -8,43 +8,12 @@
 #include <gtest/gtest.h>
 #include <opencv2/imgcodecs.hpp>
 
-#include "aruco.h"
+#include <aruco.h>
+#include "filestorage_adapter.h"
 
 #define TESTADATA_PATH "../testdata/"
 
 static bool generateResults = false;
-
-void storeMarkers(const std::vector<aruco::Marker>& markers, const std::string& path) {
-    cv::FileStorage fs(path, cv::FileStorage::WRITE);
-    fs << "Markers" << "[";
-
-    for(auto& m : markers) {
-        fs << "{";
-        fs << "id" << m.id;
-        fs << "Tvec" << m.Tvec;
-        fs << "Rvec" << m.Rvec;
-        fs << "}";
-    }
-
-    fs << "]";
-}
-
-std::vector<aruco::Marker> loadMarkers(const std::string& path) {
-    cv::FileStorage fs(path, cv::FileStorage::READ);
-
-    std::vector<aruco::Marker> ret;
-
-    for(const auto& ms : fs["Markers"]) {
-        aruco::Marker m;
-        ms["id"] >> m.id;
-        ms["Tvec"] >> m.Tvec;
-        ms["Rvec"] >> m.Rvec;
-        ret.push_back(m);
-    }
-
-    return ret;
-}
-
 
 TEST(Aruco, Single) {
     using namespace aruco;
@@ -65,16 +34,24 @@ TEST(Aruco, Single) {
     // Ok, let's detect
     MDetector.detect(InImage, Markers, CamParam, MarkerSize);
 
-    if(generateResults)
-        storeMarkers(Markers, TESTADATA_PATH "single/expected.yml");
+    int mode = cv::FileStorage::Mode(generateResults);
+    cv::FileStorage fs(TESTADATA_PATH "single/expected.yml", mode);
 
-    expected = loadMarkers(TESTADATA_PATH "single/expected.yml");
+    if(generateResults) {
+        fs << "Markers" << Markers;
+        return;
+    }
+
+    fs["Markers"] >> expected;
 
     // now check the results
     EXPECT_EQ(expected.size(), Markers.size());
 
     for (auto i = 0; i < 6; i++) {
         EXPECT_EQ(expected[i].id, Markers[i].id);
+
+        EXPECT_FLOAT_EQ(expected[i].getCenter().x, Markers[i].getCenter().x);
+        EXPECT_FLOAT_EQ(expected[i].getCenter().y, Markers[i].getCenter().y);
 
         for(auto j = 0; j < 3; j++) {
             EXPECT_FLOAT_EQ(expected[i].Tvec[j], Markers[i].Tvec[j]);
@@ -84,7 +61,42 @@ TEST(Aruco, Single) {
 }
 
 TEST(Aruco, Board) {
-    // TODO
+    using namespace aruco;
+    aruco::CameraParameters CamParam;
+    MarkerDetector MDetector;
+    vector<Marker> Markers;
+    float MarkerSize = 1;
+    BoardConfiguration TheBoardConfig;
+    BoardDetector TheBoardDetector;
+    Board TheBoardDetected, expected;
+
+    cv::Mat InImage = cv::imread(TESTADATA_PATH "board/image-test.png");
+    TheBoardConfig.readFromFile(TESTADATA_PATH "board/board_pix.yml");
+
+    CamParam.readFromXMLFile(TESTADATA_PATH "board/intrinsics.yml");
+    // resizes the parameters to fit the size of the input image
+    CamParam.resize(InImage.size());
+
+    MDetector.detect(InImage, Markers); // detect markers without computing R and T information
+    // Detection of the board
+    TheBoardDetector.detect(Markers, TheBoardConfig, TheBoardDetected, CamParam, MarkerSize);
+
+    int mode = cv::FileStorage::Mode(generateResults);
+    cv::FileStorage fs(TESTADATA_PATH "board/expected.yml", mode);
+
+    if(generateResults) {
+        fs << "Board" << TheBoardDetected;
+        return;
+    }
+
+    fs["Board"] >> expected;
+
+    // now check the results
+    EXPECT_EQ(expected.size(), TheBoardDetected.size());
+    for(int i = 0; i < 3; i++) {
+        EXPECT_FLOAT_EQ(expected.Rvec.at<float>(i), TheBoardDetected.Rvec.at<float>(i));
+        EXPECT_FLOAT_EQ(expected.Tvec.at<float>(i), TheBoardDetected.Tvec.at<float>(i));
+    }
 }
 
 TEST(Aruco, Chessboard) {
@@ -109,3 +121,20 @@ TEST(Aruco, HRM_Single) {
     // use Testdata of Single
 }
 
+TEST(ArucoPerf, Single) {
+    // ATTENTION: finish normal tests first
+    // TODO something like (attention pseudocode):
+    // auto t0 = std::chrono::steady_clock::now();
+    for(int i = 0; i < 1000; i++) {
+        // MDetector.detect(InImage, Markers, CamParam, MarkerSize);
+    }
+    // auto t = std::chrono::steady_clock::now() - t0;
+    // auto first_run = -1;
+    // if(/var/tmp/something.xx exists)
+    //     first_run = read t from /var/tmp/something.xx
+    // if(first_run > 0) {
+    //    EXPECT_LT(t, first_run);
+    // } else {
+    //    write t to file in /var/tmp/something.xx
+    // }
+}
---
+++ test/filestorage_adapter.h
@@ -0,0 +1,90 @@
+/*
+ * filestorage_adapter.h
+ *
+ *  Created on: 26.06.2015
+ *      Author: parojtbe
+ */
+
+#pragma once
+
+#include "aruco.h"
+
+namespace cv {
+inline FileStorage& operator<<(FileStorage& fs, const aruco::Marker& m) {
+    fs << "{";
+    fs << "id" << m.id;
+    fs << "Tvec" << m.Tvec;
+    fs << "Rvec" << m.Rvec;
+    fs << "corners" << "[:";
+
+    for(auto& c : m) {
+        fs << c;
+    }
+
+    fs << "]";
+    fs << "}";
+
+    return fs;
+}
+
+inline FileStorage& operator<<(FileStorage& fs, const std::vector<aruco::Marker>& markers) {
+    fs << "[";
+    for(auto& m : markers) {
+        fs << m;
+    }
+    fs << "]";
+
+    return fs;
+}
+
+inline FileStorage& operator<<(FileStorage& fs, const aruco::Board& b) {
+    fs << "{";
+    fs << "Tvec" << b.Tvec;
+    fs << "Rvec" << b.Rvec;
+    fs << "Markers" << "[";
+
+    for(auto& m : b) {
+        fs << m;
+    }
+
+    fs << "]";
+    fs << "}";
+
+    return fs;
+}
+
+inline void read(const FileNode& ms, aruco::Marker& m, const aruco::Marker& default_value = aruco::Marker()) {
+    if(ms.empty()) {
+        m = default_value;
+        return;
+    }
+
+    ms["id"] >> m.id;
+    ms["Tvec"] >> m.Tvec;
+    ms["Rvec"] >> m.Rvec;
+
+    const FileNode& corners = ms["corners"];
+    m.resize(corners.size());
+
+    for(size_t i = 0; i < m.size(); i++) {
+        corners[i] >> m[i];
+    }
+}
+
+inline void read(const FileNode& bs, aruco::Board& b, const aruco::Board& default_value = aruco::Board()) {
+    if(bs.empty()) {
+        b = default_value;
+        return;
+    }
+
+    bs["Tvec"] >> b.Tvec;
+    bs["Rvec"] >> b.Rvec;
+
+    const FileNode& markers = bs["Markers"];
+    b.resize(markers.size());
+
+    for(size_t i = 0; i < b.size(); i++) {
+        markers[i] >> b[i];
+    }
+}
+}
